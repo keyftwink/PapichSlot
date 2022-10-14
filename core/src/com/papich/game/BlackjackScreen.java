58331e3c2b7a383aaf80ca28dd93b9506a3e3263
Ixor <dsv>
2022-10-14 15:24:35 +0300

    atlas

--- core/src/com/papich/game/BlackjackScreen.java
+++ core/src/com/papich/game/BlackjackScreen.java
@@ -20,10 +20,23 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import java.util.ArrayList;
+
 public class BlackjackScreen implements Screen {
 
     TextButton.TextButtonStyle textButtonStyle;
     BitmapFont font;
+    BitmapFont moneyFont;
+    public static ArrayList<String> croupierDeck = new ArrayList<>();
+    public static ArrayList<String> playerDeck = new ArrayList<>();
+    public static int cardsCounter = 0;
+
+    public static boolean isCroupierReady;
+    public static boolean isPlayerReady;
+    public static String[] deck = new String[52];
+    public static int money = 10000;
+    public static int bet;
+    String[] cards = new String[]{"2 spade","2 club","2 diamond","2 heart","3 spade","3 club","3 diamond","3 heart","4 spade","4 club","4 diamond","4 heart","5 spade","5 club","5 diamond","5 heart","6 spade","6 club","6 diamond","6 heart","7 spade","7 club","7 diamond","7 heart","8 spade","8 club","8 diamond","8 heart","9 spade","9 club","9 diamond","9 heart","10 spade","10 club","10 diamond","10 heart","J spade","J club","J diamond","J heart","Q spade","Q club","Q diamond","Q heart","K spade","K club","K diamond","K heart","A spade","A club","A diamond","A heart"};
     private SpriteBatch batch;
     protected Stage stage;
     private Viewport viewport;
@@ -35,19 +48,19 @@ public class BlackjackScreen implements Screen {
     public BlackjackScreen() {
         atlas = new TextureAtlas("skin.atlas");
         skin = new Skin();
-        blackJackTable = new Texture(Gdx.files.internal("blackjacktable.jpg"));
+        blackJackTable = new Texture(Gdx.files.internal("BlackJackAssets/blackjacktable.jpg"));
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("myFont.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 30;
-        Color color = Color.BLACK;
-        parameter.color = color;
+        parameter.color = new Color(0xe6e682AA);
         font = generator.generateFont(parameter);
-        font.setColor(Color.BLACK);
         skin.addRegions(atlas);
         textButtonStyle = new TextButton.TextButtonStyle();
         textButtonStyle.font = font;
         textButtonStyle.up = skin.getDrawable("button");
-
+        parameter.color = Color.WHITE;
+        parameter.size = 50;
+        moneyFont = generator.generateFont(parameter);
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
         viewport = new FitViewport(1920, 1080, camera);
@@ -87,6 +100,8 @@ public class BlackjackScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(blackJackTable,0,0);
+
+        moneyFont.draw(batch, String.valueOf(money),1500,100);
         batch.end();
         stage.act();
         stage.draw();
